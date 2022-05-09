84deb4ead216077f22a3162c477c503f4ab0586a
Tanvi Bhosale <65451201+tanvib07@users.noreply.github.com>
2022-05-09 06:35:02 +0530

    Update SDES-2.py

--- SDES-2.py
+++ SDES-2.py
@@ -3,41 +3,16 @@
 # K1 xor with new key
 # K2 xor with new key
 
-def per8(text):
+def per(text,per):
     t = list(text)
     s=" "
-    #6 3 7 4 8 5 10 9
     print("------------------------------")
     p = list()
-    p.insert(0, t[7])
-    p.insert(1, t[2])
-    p.insert(2, t[6])
-    p.insert(3, t[3])
-    p.insert(4, t[5])
-    p.insert(5, t[1])
-    p.insert(6, t[4])
-    p.insert(7, t[0])
-    print("After Permutation")
-    print(s.join(p))
-    print(" ")
-    print("------------------------------")
-    return p
 
-def split_exp_per(p_text):
-    t = p_text[4:]
-    p = list()
-    s=" "
-    #4 1 2 3 2 3 4 1
-    print("Right half is "+str(s.join(t)))
-    p.insert(0, t[1])
-    p.insert(1, t[2])
-    p.insert(2, t[3])
-    p.insert(3, t[0])
-    p.insert(4, t[2])
-    p.insert(5, t[1])
-    p.insert(6, t[3])
-    p.insert(7, t[0])
-    print("After Expantion and Permutation on right half ")
+    for i in per:
+        p.append(t[i - 1])
+
+    print("After Permutation")
     print(s.join(p))
     print(" ")
     print("------------------------------")
@@ -51,13 +26,16 @@ if __name__ == "__main__":
     k2 = list(map(int,input()))
     print("Enter plain text")
     text = input()
-    p_text = per8(text)
-    output = split_exp_per(p_text)
+    p8 = [2,6,3,1,4,8,5,7]
+    exp=[4,1,2,3,2,3,4,1]
+
+    p_text = per(text, p8)
+    t = p_text[4:]
+    output = per(t, exp)
     output = [int(i) for i in output]
     s=" "
     #XOR with k1
     result1 = list(map(str, (a ^ b for a, b in zip(k1, output))))
-
     print("XOR with K1 : "+str(s.join(result1)))
     print(" ")
     print("------------------------------")
